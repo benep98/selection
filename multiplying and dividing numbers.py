ea3b1f8dde1b1377e344ab1059c2039e5c486d5c
benep98 <40139@longroad.ac.uk>
2014-09-29 11:42:35 +0100

    Delete multiplying and dividing numbers.py

--- multiplying and dividing numbers.py
+++
@@ -1,8 +0,0 @@
-first_number = int(input("Please enter your first number"))
-second_number = int(input("Please enter your second number"))
-third_number = int(input("Please enter your third number"))
-
-answer1 = first_number * second_number
-answer2 = answer1 / third_number
-print(answer2)
-print("{0:.0f}".format(answer2))
