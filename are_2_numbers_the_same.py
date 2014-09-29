51231c487fb73024831c8578125d9ccafbd43e4c
Benjamin Pope <40139@longroad.ac.uk>
2014-09-29 12:15:44 +0100

    selection exercises

---
+++ are_2_numbers_the_same.py
@@ -0,0 +1,6 @@
+number1 = input("Please enter a number")
+number2 = input("Please enter a second number")
+if number1 == number2:
+    print("The two numbers are the same")
+else:
+    print("The two numbers are different")
