f5a7d5aa008c8a2d9271c3ba880efa600e154798
Benjamin Pope <40139@longroad.ac.uk>
2014-09-29 11:41:24 +0100

    deleting

--- adding 4 numbers.py
+++
@@ -1,7 +0,0 @@
-first_number = int(input("Please enter your first number"))
-second_number = int(input("Please enter your second number"))
-third_number = int(input("Please enter your third number"))
-fourth_number = int(input("Please enter your fourth number"))
-answer = first_number + second_number + third_number + fourth_number
-
-print(answer)
--- mod and div.py
+++
@@ -1,9 +0,0 @@
-first_number = int(input("Please enter your first number"))
-second_number = int(input("Please enter your second number"))
-
-answer = first_number // second_number
-answer2 = first_number % second_number
-
-
-print("{0} divides by {1} a number of {2} times".format(first_number,second_number,answer))
-print("The remainder of {0} divided by {1} is {2}".format(first_number,second_number,answer2))
